ed43c3dbeb44f0527a4547245e027eac35c2d1a4
Marco Wang <m.aesophor@gmail.com>
2020-10-27 14:24:31 +0800

    parse.h: enlarge parameter_strings_long
    This patch is necessary to fuzz imagemagick

--- parse.h
+++ parse.h
@@ -5,7 +5,7 @@
 #include <string.h>
 
 #define parameter_array_size 100
-#define parameter_strings_long 60
+#define parameter_strings_long 100
 #define variable_array_size 30
 
 struct parameter_list
